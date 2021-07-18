6a21be964ffe6e7b3fcf72b612ea5d4366016de9
horsewin <mamamatz@gmail.com>
2021-07-19 02:03:04 +0900

    fix: docker-ecs

--- hoge.sh
+++ hoge.sh
@@ -15,9 +15,10 @@ if [ $ROLE_STATUS -ne 200 ]; then
     exit 1
 fi
 
+
 cd ${APP_DIR}
 # インストール時にPATHの記述順序でエラーが出る対策
-TMP_PATH=$(echo $PATH | sed -e 's#/usr/local/bin:##g') && PATH=/usr/local/bin:$TMP_PATH
+TmpPATH=$(echo $PATH | sed -e 's#/usr/local/bin:##g') && PATH=/usr/local/bin:$TmpPATH
 
 # Docker ECSインテグレーションのダウンロード
 curl -Lo docker-ecs.sh https://raw.githubusercontent.com/docker/compose-cli/main/scripts/install/install_linux.sh
@@ -29,7 +30,16 @@ sed -i -e 's/latest/44054930/g' docker-ecs.sh
 # インストールに失敗するときは前述のsedコマンドをスキップしてお試しください
 sh docker-ecs.sh
 
-# これ以降で必要なツールのインストール
+# IAMロールのプロファイルを取得できないため手動で設定
 sudo yum install -y jq
+ROLE_NAME=$(curl -s ${METADATA_URL})
+export AWS_SESSION_TOKEN=$(curl -s ${METADATA_URL}/${ROLE_NAME} | jq -r .Token)
+export AWS_SECRET_ACCESS_KEY=$(curl -s ${METADATA_URL}/${ROLE_NAME} | jq -r .SecretAccessKey)
+export AWS_ACCESS_KEY_ID=$(curl -s ${METADATA_URL}/${ROLE_NAME} | jq -r .AccessKeyId)
+export AWS_DEFAULT_REGION=ap-northeast-1
+
+# ECRの認証情報の取得
+export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
+aws ecr --region ap-northeast-1 get-login-password | docker login --username AWS --password-stdin https://${AWS_ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/sbcntr-backend
 
 echo "Setup complete!"
